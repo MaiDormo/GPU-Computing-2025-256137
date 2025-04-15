47b3701ca62066cfc5324e4727ff81e2278e8c93
maidormo <elia.gatti01@gmail.com>
2025-04-15 11:57:38 +0200

    added shared memory matrix add

--- test/sum_shared.cu
+++ test/sum_shared.cu
@@ -22,8 +22,6 @@ __global__ void add_shared_memory(const int n, dtype *x, dtype *y) {
     int idx = threadIdx.x + blockDim.x * blockIdx.x;
 
     const int dim = n < 36 ? n : 36;
-    const int x_chunk_dim = dim;
-    const int y_chunk_dim = dim;
 
     //load to shared memory
     const int pos_in_shared_vec = idx % dim;
@@ -32,7 +30,7 @@ __global__ void add_shared_memory(const int n, dtype *x, dtype *y) {
     // each thread computes
     sharedData[pos_in_shared_vec + dim] = sharedData[pos_in_shared_vec + dim] + sharedData[pos_in_shared_vec];
     // synch thread
-    __syncthreads();
+    __syncthreads(); //reset values
     //send_back result
     y[idx] = sharedData[pos_in_shared_vec + dim];
 }
@@ -46,123 +44,87 @@ __global__ void add_shared_memory_and_2d_thread_blocks(const int n, dtype *x, dt
 }
 
 
-void bench(int len, cudaEvent_t start, const int grid_sizes[], const int block_sizes[], int N, dtype *x, dtype *y, cudaEvent_t end, float times[]) {
+void add_shared_memory_bench(const int runs, cudaEvent_t start, cudaEvent_t end, 
+    const int thread_per_block, const int block_num, int N, dtype *x, dtype *y , float times[]) {
     float millis = 0.0;
-    for (int i = 0; i < len; i++) {
-        for (int j = 0; j < len; j++) {
-            cudaEventRecord(start);
-            add_linear_access<<<grid_sizes[j], block_sizes[i]>>>(N, x, y);
-            cudaEventRecord(end);
-            cudaEventSynchronize(end);
-            cudaEventElapsedTime(&millis, start, end);
-            times[j + i * len] = millis;
-        }
+    for (int i = 0; i < runs; i++) {
+        cudaEventRecord(start);
+        add_shared_memory<<<block_num, thread_per_block>>>(N, x, y);
+        cudaEventRecord(end);
+        cudaEventSynchronize(end);
+        cudaEventElapsedTime(&millis, start, end);
+        times[runs] = millis;
     }
 }
 
+void print_stat(const float times[], const int runs, const int N, const double values[], const char * unit) {
 
-void linear_access_bench(cudaEvent_t start, cudaEvent_t end, const int grid_sizes[], 
-    const int block_sizes[], float times[], int len, dtype* x, dtype* y, int N) {
-    
-    bench(len, start, grid_sizes, block_sizes, N, x, y, end, times);
-}
-
-void consecutive_access_bench(cudaEvent_t start, cudaEvent_t end, const int grid_sizes[], 
-    const int block_sizes[], float times[], int len, dtype* x, dtype* y, int N) {
-    
-    bench(len, start, grid_sizes, block_sizes, N, x, y, end, times);
-}
-
-void print_stat(const int grid_sizes[], const int block_sizes[], const float times[], const int len, const int N,
-    const double values[], const char * unit) {
-    // Header row with grid sizes
-    std::cout << std::left << std::setw(12) << "Block/Grid";
-    for (int j = 0; j < len; j++) {
-        std::cout << "| " << std::left << std::setw(10) << grid_sizes[j] << " ";
-    }
-    std::cout << std::endl;
     
     // Print separator
     std::cout << std::string(12, '-');
-    for (int j = 0; j < len; j++) {
+    for (int j = 0; j < runs; j++) {
         std::cout << "+" << std::string(12, '-');
     }
     std::cout << std::endl;
 
     // Print results for each block size
-    for (int i = 0; i < len; i++) {
-        std::cout << std::left << std::setw(12) << block_sizes[i];
-        for (int j = 0; j < len; j++) {
-            if (values == NULL) {
-                std::cout << "| " << std::fixed << std::setprecision(2)
-                     << std::setw(8) << times[i*len+j] << unit << " ";
-            } else {
-                std::cout << "| " << std::fixed << std::setprecision(2)
-                         << std::setw(6) << values[i*len+j] << unit << " ";
-            }
+    for (int i = 0; i < runs; i++) {
+        if (values == NULL) {
+            std::cout << "| " << std::fixed << std::setprecision(2)
+                 << std::setw(8) << times[i] << unit << " ";
+        } else {
+            std::cout << "| " << std::fixed << std::setprecision(2)
+                     << std::setw(6) << values[i] << unit << " ";
         }
-        std::cout << std::endl;
     }
     std::cout << std::endl;
 }
 
 
-void compute_result(const int grid_sizes[], const int block_sizes[], 
-    const float times[], int len, int N, const char* test_name) {
+void compute_result(const float times[], int runs, int N, const char* test_name) {
     
-    size_t bytes_read = N * sizeof(dtype) * 2;
-    size_t bytes_written = N * sizeof(dtype);
+    size_t bytes_read = N * N * sizeof(dtype) * 2;
+    size_t bytes_written = N * N * sizeof(dtype);
     size_t total_bytes = bytes_read + bytes_written;
 
     std::cout << "\n===== " << test_name << " Results =====\n";
 
-    double values[len*len];
+    double values[runs];
 
-    print_stat(grid_sizes,block_sizes,times,len,N,NULL, "ms");
+    print_stat(times,runs,N,NULL,"ms");
 
     //compute the bandwidth (GB/s)
-    for (int i = 0; i < len; i++) {
-        for (int j = 0; j < len; j++) {
-            values[i*len+j] = total_bytes /  (times[i*len+j] * 1.0e6); // GB/s
-        }
+    for (int i = 0; i < runs; i++) {
+        values[i] = total_bytes /  (times[i] * 1.0e6); // GB/s
     }
 
-    print_stat(grid_sizes,block_sizes,times,len,N,values, "GB/s");
+    print_stat(times,runs,N,values,"GB/s");
 
     //compute the GFLOPS
-    for (int i = 0; i < len; i++) {
-        for (int j = 0; j < len; j++) {
-            values[i*len+j] = N / (times[i*len+j] * 1.0e6);
-        }
+    for (int i = 0; i < runs; i++) {
+        values[i] = (N * N) / (times[i] * 1.0e6);
     }
 
-    print_stat(grid_sizes,block_sizes,times,len,N,values, "GFLOPS");
+    print_stat(times,runs,N,values,"GFLOPS");
     
 }
 
 
-void linear_compute_result(const int grid_sizes[], const int block_sizes[], 
-    const float linear_times[], int len, int N) {
-    compute_result(grid_sizes, block_sizes, linear_times, len, N, "Linear");
-}
-
-void consecutive_compute_result(const int grid_sizes[], const int block_sizes[], 
-    const float consecutive_times[], int len, int N) {
-    compute_result(grid_sizes, block_sizes, consecutive_times, len, N, "Consecutive");
+void add_shared_mem_compute_result(const float shared_mem_times[], int runs, int N) {
+    compute_result(shared_mem_times, runs, N, "Shared Memory");
 }
 
 
 
 int main(int argc, char **argv) {
 
-    const int N = 4096 * 16;
+    const int N = 4096 * 4;
     
     // For benchmarking
-    const int len = 6;
-    const int grid_sizes[] = {1,3,7,14,28,56};
-    const int block_sizes[] = {32,64,128,256,512,1024};
-    float linear_times[len*len];
-    float consecutive_times[len*len];
+    const int runs = 6;
+    const int thread_per_block = 36*36;
+    const int block_nums = (N*N + thread_per_block - 1) / thread_per_block;
+    float times[runs];
     
     // Allocate Unified Memory accessible from CPU or GPU
     dtype *x, *y;
@@ -184,24 +146,12 @@ int main(int argc, char **argv) {
 
     //---------------BENCHMARKING-------------------
 
-    linear_access_bench(start,end,grid_sizes,block_sizes,linear_times,len,x,y,N);
+    add_shared_memory_bench(runs,start,end,thread_per_block,block_nums,N,x,y,times);
 
-    //reset values
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            int pos = i * N + j;
-            x[pos] = pos;
-            x[pos] = pos; 
-        }
-    }
-
-    consecutive_access_bench(start,end,grid_sizes,block_sizes,consecutive_times,len,x,y,N);
     
     //---------------COMPUTE RESULTS------------------
 
-    linear_compute_result(grid_sizes,block_sizes,linear_times,len,N);
-    printf("\n");
-    consecutive_compute_result(grid_sizes,block_sizes,consecutive_times,len,N);
+    add_shared_mem_compute_result(times,runs,N);
     printf("\n");
 
     //------------------------------------------------
